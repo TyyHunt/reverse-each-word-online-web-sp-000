6a9be1aaa31f7abe8ea192e1031f4ad2bfbe10f1
Tyler Hunt <tyhunt04@gmail.com>
2019-02-17 01:31:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,9 @@ def reverse_each_word(sentence)
   original_array.each do |word|
     word.reverse
   end
-  original_array.join(" ")
+
+  original_array.collect do |word|
+    word
+  end
+  original_array
 end
